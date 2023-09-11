a15fe4dae8d83ffc354e4235ee32461ed69bd840
Shubh Agrawal <shubhagrawal30@gmail.com>
2023-09-11 13:19:43 -0400

    add exe

--- FASTphysics/exe.py
+++ FASTphysics/exe.py
@@ -1,8 +1,13 @@
-import os, sys, server
+import os, sys
 import streamlit.web.cli as stcli
 
 if __name__ == "__main__":
+    if getattr(sys, 'frozen', False):
+        application_path = os.path.join(os.path.dirname(sys.executable))
+    else:
+        application_path = os.path.dirname(os.path.abspath(__file__))
     sys.argv = ["streamlit", "run", \
-                os.path.join(os.path.dirname(__file__), "server.py"), \
-                "--browser.gatherUsageStats=false"]
+                os.path.join(application_path, "server.py"), \
+                "--browser.gatherUsageStats=false", "--server.enableCORS=true",
+                "--server.enableXsrfProtection=false"]
     sys.exit(stcli.main())
