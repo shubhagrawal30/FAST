d8b833e1f754e12cd17e9b0288184f4226a206aa
Shubh Agrawal <shubh@ist-14012.apn.wlan.private.upenn.edu>
2023-10-09 16:50:44 -0400

    added teacher interface

--- FASTphysics/admin/server.py
+++ FASTphysics/admin/server.py
@@ -2,14 +2,38 @@
 # an app that inputs entries through streamlit forms and prints the entries
 
 import streamlit as st
+from google.cloud import firestore
+from google.oauth2 import service_account
+import json
+
+collection_name, document_name = "prompts", "demo"
+subject_field_name, initp_field_name = "subject", "init"
 
 if __name__ == "__main__":
+    firestore_key = json.loads(st.secrets["FIRESTORE_KEY"])
+    creds = service_account.Credentials.from_service_account_info(firestore_key)
+    db = firestore.Client(credentials=creds)
+    
+    # load current values from the database
+    doc_ref = db.collection(collection_name).document(document_name)
+    doc = doc_ref.get()
+    if doc.exists:
+        subject = doc.get(subject_field_name)
+        initp = doc.get(initp_field_name)
+    else:
+        subject = "physics"
+        initp = "You are a Friendly Awesome Smart Tutor for physics!"
+    
     st.title("FASTphysics Admin")
-    
     # entry box for the subject
-    subject = st.text_input("Subject", "physics")
-    
+    subject = st.text_input("Subject", subject)
     # entry box for the initialization prompt
-    initp = st.text_input("Prompt", f"You are a Friendly Awesome Smart Tutor for {subject}!")
+    initp = st.text_input("Prompt", initp)
     
-    print(subject, initp)
+    # set up the inputs in the database
+    doc_ref = db.collection(collection_name).document(document_name)
+    doc = doc_ref.get()
+    if doc.exists:
+        doc_ref.update({subject_field_name: subject, initp_field_name: initp})
+    else:
+        doc_ref.set({subject_field_name: subject, initp_field_name: initp})
--- FASTphysics/utils/json-to-toml.py
+++ FASTphysics/utils/json-to-toml.py
@@ -8,16 +8,16 @@ current_file_path = os.path.dirname(os.path.realpath(__file__))
 key_file = os.path.join(current_file_path, "../.streamlit/firestore.json")
 output_file = os.path.join(current_file_path, "../.streamlit/secrets.toml")
 
-# check if textkey is already in secrets.toml, if yes then exit
+# check if FIRESTORE_KEY is already in secrets.toml, if yes then exit
 with open(output_file) as target:
-    if "textkey" in target.read():
-        print("textkey already in secrets.toml")
+    if "FIRESTORE_KEY" in target.read():
+        print("FIRESTORE_KEY already in secrets.toml")
         exit()
 
 with open(key_file) as json_file:
     json_text = json_file.read()
 
-config = {"textkey": json_text}
+config = {"FIRESTORE_KEY": json_text}
 toml_config = toml.dumps(config)
 
 with open(output_file, "a") as target:
